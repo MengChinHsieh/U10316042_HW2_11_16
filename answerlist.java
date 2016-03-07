aa6d64a1d801b3cf246b2ecc6fe7e2acd895baf0
MengChinHsieh <lh1202@yahoo.com.tw>
2016-03-07 20:14:32 +0800

    Create answerlist.java

---
+++ answerlist.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+class answerlist{
+    
+    private int answer;
+    
+    
+    answerlist(int answer){
+        this.answer = answer;
+        
+    }
+    
+    void setanswer(int answer){
+        this.answer = answer;
+    }
+    
+    int getanswer(){
+        return answer;
+    }
+    
+    
+}
